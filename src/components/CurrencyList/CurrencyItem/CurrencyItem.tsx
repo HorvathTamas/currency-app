b4147fd0c2d7b9533cdeb90d5d72c421398c8c46
Horváth Tamás <horvath.tamas.norbert93@gmail.com>
2021-10-11 02:38:42 +0200

    [feat] Add responsive UI

--- src/components/CurrencyList/CurrencyItem/CurrencyItem.tsx
+++ src/components/CurrencyList/CurrencyItem/CurrencyItem.tsx
@@ -13,11 +13,15 @@ const CurrencyItem: FunctionComponent<CurrencyItemProps> = ({ currencyItem }) =>
   const { currency, exchangeRate, nameI18N } = currencyItem;
   return (
     <Paper elevation={6} className="currency-item">
-      <FlagIcon currency={currency} />
-      <div>{nameI18N}</div>
-      <div>{`Buy: ${roundNum(exchangeRate.buy)} ${currency}`}</div>
-      <div>{`Mid: ${roundNum(exchangeRate.middle)} ${currency}`}</div>
-      <div>{`Sell: ${roundNum(exchangeRate.sell)} ${currency}`}</div>
+      <div className="currency-item__flag-container">
+        <FlagIcon currency={currency} />
+      </div>
+      <div className="currency-item__name">{nameI18N}</div>
+      <div className="currency-item__rates">
+        <div>{`Buy: ${roundNum(exchangeRate.buy)} ${currency}`}</div>
+        <div>{`Mid: ${roundNum(exchangeRate.middle)} ${currency}`}</div>
+        <div>{`Sell: ${roundNum(exchangeRate.sell)} ${currency}`}</div>
+      </div>
     </Paper>
   );
 };
